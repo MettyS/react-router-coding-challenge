8bfcb6aa2e198747704acaaf5919f29551988713
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-10-12 13:35:21 -0400

    stripped example down to challenge only

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import './App.css';
-import Error from './Component/Error'
-import Child from './Component/Child'
-import GrandChild from './Component/GrandChild'
-import Home from './Component/Home'
-import Navigation from './Component/Navigation'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className='app'>
-      <BrowserRouter>
-        <h2>React Router Practice</h2>
-        {/* un-routed component */}
-        <Navigation />
-        <Switch>
-          {/* statis routes */}
-          <Route exact path='/' component={Home} />
-          <Route path='/child' component={Child} />
-          {/* dynamic route */}
-          <Route path="/grand-child/:meta" component={GrandChild} />
-          {/* error route */}
-          <Route component={Error}/>
-        </Switch>
-      </BrowserRouter>
+      
     </div>
   );
 }
